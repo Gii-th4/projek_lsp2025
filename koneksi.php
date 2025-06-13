e23b6a19a0293db911bd8216592b2fd083dc8680
Gii-th4 <anggitadewlestari@gmail.com>
2025-06-13 12:10:37 +0700

    13th

--- koneksi.php
+++ koneksi.php
@@ -4,10 +4,10 @@ use Dom\Mysql;
 
 session_start();
 
-$host = "localhost"; 
-$user = "root";      
-$pass = "root";          
-$db   = "pinjamkamera";  
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$db   = "pinjamkamera";
 
 // Membuat koneksi
 $koneksi = mysqli_connect($host, $user, $pass, $db);
@@ -19,15 +19,15 @@ if (!$koneksi) {
 
 //tambah barang
 
-if(isset($_POST['addkamera'])){
+if (isset($_POST['addkamera'])) {
     $nama_kamera = $_POST['nama_kamera'];
     $merk = $_POST['merk'];
     $stock = $_POST['stock'];
 
     $addtotable = mysqli_query($koneksi, "insert into stock (nama_kamera, merk, stock) values('$nama_kamera', '$merk', '$stock')");
-    if($addtotable){
+    if ($addtotable) {
         header('location:index.php');
-    }else {
+    } else {
         echo "Gagal";
         header('gagal');
     }
@@ -35,7 +35,7 @@ if(isset($_POST['addkamera'])){
 
 //tambah kamera masuk
 
-if(isset($_POST['kameramasuk'])){
+if (isset($_POST['kameramasuk'])) {
     $id_kamera = $_POST['kameranya'];
     $penerima = $_POST['penerima'];
     $quantity = $_POST['quantity'];
@@ -48,9 +48,9 @@ if(isset($_POST['kameramasuk'])){
 
     $addtomasuk = mysqli_query($koneksi, "insert into masuk (id_kamera, keterangan, quantity) values('$id_kamera', '$penerima' , '$quantity')");
     $update_stock = mysqli_query($koneksi, "UPDATE stock SET stock = '$tambahstockskrgdgquantity' WHERE id_kamera = '$id_kamera'");
-    if($addtomasuk&&$update_stock){
+    if ($addtomasuk && $update_stock) {
         header('location:index.php');
-    }else {
+    } else {
         echo "Gagal";
         header('gagal');
     }
@@ -58,7 +58,7 @@ if(isset($_POST['kameramasuk'])){
 
 //tambah kamera keluar
 
-if(isset($_POST['kamerakeluar'])){
+if (isset($_POST['kamerakeluar'])) {
     $id_kamera = $_POST['kameranya'];
     $penerima = $_POST['penerima'];
     $quantity = $_POST['quantity'];
@@ -71,12 +71,168 @@ if(isset($_POST['kamerakeluar'])){
 
     $addtokeluar = mysqli_query($koneksi, "insert into keluar (id_kamera, penerima, quantity) values('$id_kamera', '$penerima' , '$quantity')");
     $update_stock = mysqli_query($koneksi, "UPDATE stock SET stock = '$tambahstockskrgdgquantity' WHERE id_kamera = '$id_kamera'");
-    if($addtokeluar&&$update_stock){
+    if ($addtokeluar && $update_stock) {
         header('location:keluar.php');
-    }else {
+    } else {
         echo "Gagal";
         header('location:keluar.php');
     }
 };
+
+//update info barang
+if (isset($_POST['updatebarang'])) {
+    $id_kamera  = $_POST['idb'];
+    $nama_kamera = $_POST['nama_kamera'];
+    $merk = $_POST['merk'];
+
+    $update = mysqli_query($koneksi, "update stock set nama_kamera='$nama_kamera', merk='$merk' where id_kamera ='$id_kamera'");
+    if ($update) {
+        header('location:index.php');
+    } else {
+        echo 'Gagal';
+        header('location:index.php');
+    };
+};
+
+//hapus barang dari stock
+if (isset($_POST['hapusbarang'])) {
+    $idb = $_POST['idb'];
+
+    $hapus = mysqli_query($koneksi, "delete from stock where id_kamera='$idb'");
+    if ($hapus) {
+        header('location:index.php');
+    } else {
+        echo 'Gagal';
+        header('location:index.php');
+    };
+};
+
+//ubah data barang masuk
+if(isset($_POST['updatebarangmasuk'])){
+    $idb = $_POST['idb'];
+    $idm = $_POST['idm'];
+    $keterangan = $_POST['keterangan'];
+    $quantity = $_POST['quantity'];
+
+    $lihatstock = mysqli_query($koneksi, "SELECT * FROM stock where id_kamera='$idb'");
+    $stocknya = mysqli_fetch_array($lihatstock);
+    $stockskrg = $stocknya['stock'];
+
+    $quantityskrg = mysqli_query($koneksi, "SELECT * FROM  masuk where id_masuk='$idm'");
+    $quantityny =  mysqli_fetch_array($quantityskrg);
+    $quantityskrg = $quantityny['quantity'];
+
+    if($quantity>$quantityskrg){
+        $selisih = $quantity - $quantityskrg;
+        $kurangin = $stockskrg - $selisih;
+        $kurangistocknya = mysqli_query($koneksi, "UPDATE stock set stock='$kurangin' WHERE id_kamera='$idb'");
+        $updatenya = mysqli_query($koneksi,"UPDATE masuk set quantity='$quantity', keterangan='$keterangan' WHERE id_masuk='$idm'");
+        if($kurangistocknya&&$updatenya){
+            header('location:masuk.php');
+        }else{
+            echo "Gagal";
+            header('location:masuk.php');
+        }
+    }else{
+        $selisih = $quantityskrg - $quantity;
+        $kurangin = $stockskrg + $selisih;
+        $kurangistocknya = mysqli_query($koneksi, "UPDATE stock set stock='$kurangin' WHERE id_kamera='$idb'");
+        $updatenya = mysqli_query($koneksi,"UPDATE masuk set quantity='$quantity', keterangan='$keterangan' WHERE id_masuk='$idm'");
+        if($kurangistocknya&&$updatenya){
+            header('location:masuk.php');
+        }else{
+            echo "Gagal";
+            header('location:masuk.php');
+        }
+    }
+}
+
+//hapus barang masuk
+if(isset($_POST['hapusbarangmasuk'])){
+    $idb = $_POST['idb'];
+    $quantity = $_POST['quantity'];
+    $idm = $_POST['idm'];
+
+    $getdatastock = mysqli_query($koneksi, "SELECT * FROM stock where id_kamera='$idb'");
+    $data = mysqli_fetch_array($getdatastock);
+    $stock = $data['stock'];
+
+    $selisih = $stock - $quantity;
+
+
+    $update = mysqli_query($koneksi, "UPDATE stock set stock='$selisih' where id_kamera='$idb'");
+    $hapusdata = mysqli_query($koneksi, "DELETE from  masuk where id_masuk='$idm'");
+
+    if($update&&$hapusdata){
+        header('location:masuk.php');
+    }else{
+         header('location:masuk.php');
+    }
+}
+
+//ubah data  barang keluar
+
+if(isset($_POST['updatebarangkeluar'])){
+    $idb = $_POST['idb'];
+    $idm = $_POST['idk'];
+    $penerima = $_POST['penerima'];
+    $quantity = $_POST['quantity'];
+
+    $lihatstock = mysqli_query($koneksi, "SELECT * FROM stock where id_kamera='$idb'");
+    $stocknya = mysqli_fetch_array($lihatstock);
+    $stockskrg = $stocknya['stock'];
+
+    $quantityskrg = mysqli_query($koneksi, "SELECT * FROM  keluar where id_keluar='$idk'");
+    $quantityny =  mysqli_fetch_array($quantityskrg);
+    $quantityskrg = $quantityny['quantity'];
+
+    if($quantity>$quantityskrg){
+        $selisih = $quantity - $quantityskrg;
+        $kurangin = $stockskrg - $selisih;
+        $kurangistocknya = mysqli_query($koneksi, "UPDATE stock set stock='$kurangin' WHERE id_kamera='$idb'");
+        $updatenya = mysqli_query($koneksi,"UPDATE keluar set quantity='$quantity', penerima='$penerima' WHERE id_keluar='$idk '");
+        if($kurangistocknya&&$updatenya){
+            header('location:keluar.php');
+        }else{
+            echo "Gagal";
+            header('location:keluar.php');
+        }
+    }else{
+        $selisih = $quantityskrg - $quantity;
+        $kurangin = $stockskrg + $selisih;
+        $kurangistocknya = mysqli_query($koneksi, "UPDATE stock set stock='$kurangin' WHERE id_kamera='$idb'");
+        $updatenya = mysqli_query($koneksi,"UPDATE keluar set quantity='$quantity', penerima='$penerima' WHERE id_keluar='$idk'");
+        if($kurangistocknya&&$updatenya){
+            header('location:keluar.php');
+        }else{
+            echo "Gagal";
+            header('location:keluar.php');
+        }
+    }
+}
+
+//hapus bbarang keluar
+if(isset($_POST['hapusbarangkeluar'])){
+    $idb = $_POST['idb'];
+    $quantity = $_POST['quantity'];
+    $idk = $_POST['idm'];
+
+    $getdatastock = mysqli_query($koneksi, "SELECT * FROM stock where id_kamera='$idb'");
+    $data = mysqli_fetch_array($getdatastock);
+    $stock = $data['stock'];
+
+    $selisih = $stock +  $quantity;
+
+
+    $update = mysqli_query($koneksi, "UPDATE stock set stock='$selisih' where id_kamera='$idb'");
+    $hapusdata = mysqli_query($koneksi, "DELETE from  keluar where id_keluar='$idk'");
+
+    if($update&&$hapusdata){
+        header('location:keluar.php');
+    }else{
+         header('location:keluar.php');
+    }
+}
+
 
 ?>
--- masuk.php
+++ masuk.php
@@ -7,35 +7,37 @@ require 'tes-koneksi.php';
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Barang Masuk</title>
-        <link href="css/styles.css" rel="stylesheet" />
-        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
-        <!-- BOOSTRAP CSS -->
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <!-- MY CSS -->
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.php"> Welcome Adel</a>
-            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-            <!-- Navbar-->
-        </nav>
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <a class="nav-link" href="index.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Stock  Kamera
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Barang Masuk</title>
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+    <!-- BOOSTRAP CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- MY CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php"> Welcome Adel</a>
+        <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+        <!-- Navbar-->
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <a class="nav-link" href="index.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Stock Kamera
                             <a class="nav-link" href="masuk.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Barang Masuk
@@ -47,20 +49,20 @@ require 'tes-koneksi.php';
                             <a class="nav-link" href="logout.php">
                                 Logout
                             </a>
-                        </div>
                     </div>
-                    <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Start Bootstrap
-                    </div>
-                </nav>
-            </div>
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid">
-                        <h1 class="mt-4">Barang Masuk</h1>
-                        
-                        <div class="card mb-4">
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    Start Bootstrap
+                </div>
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid">
+                    <h1 class="mt-4">Barang Masuk</h1>
+
+                    <div class="card mb-4">
 
                         <div class="mb-3">
                             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -81,66 +83,121 @@ require 'tes-koneksi.php';
                                             <th>Tanggal</th>
                                             <th>Nama Kamera</th>
                                             <th>Jumlah</th>
-                                            <th>Stock</th>
+                                            <th>keterangan</th>
+                                            <th>Aksi</th>
 
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        
+
                                         <?php
-                                            $ambilsemuadatanya = mysqli_query($koneksi, "SELECT * FROM masuk m, stock s where s.id_kamera = m.id_kamera");
-                                            while($data = mysqli_fetch_array($ambilsemuadatanya)){
-                                                $tanggal = $data['tanggal'];
-                                                $nama_kamera = $data['nama_kamera'];
-                                                $quantity = $data['quantity'];
-                                                $keterangan = $data['keterangan'];
-                                            };
-                                            
-                                            ?>
+                                        $ambilsemuadatanya = mysqli_query($koneksi, "SELECT * FROM masuk m, stock s WHERE s.id_kamera = m.id_kamera");
+                                        while ($data = mysqli_fetch_array($ambilsemuadatanya)) {
+                                            $idb = $data['id_kamera'];
+                                            $idm = $data['id_masuk'];
+                                            $tanggal = $data['tanggal'];
+                                            $nama_kamera = $data['nama_kamera'];
+                                            $quantity = $data['quantity'];
+                                            $keterangan = $data['keterangan'];
+                                        ?>
                                             <tr>
-                                                <td><?=$tanggal;?></td>
-                                                <td><?=$nama_kamera;?></td>
-                                                <td><?=$quantity;?></td>
-                                                <td><?=$keterangan;?></td>
-                                            </tr>      
-                                            <?php
-                                            
-                                            ?>
+                                                <td><?= $tanggal; ?></td>
+                                                <td><?= $nama_kamera; ?></td>
+                                                <td><?= $quantity; ?></td>
+                                                <td><?= $keterangan; ?></td>
+                                                 <td>
+                                                    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit<?= $idm; ?>">Edit</button>
+                                                   
+                                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete<?=$idm;?>">Delete</button>
+                                                </td>
+                                            </tr>
+
+                                            <div class="modal fade" id="edit<?=$idm;?>">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Barang</h1>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+
+                                                        <form method="post">
+                                                            <div class="modal-body">
+                                                            
+                                                                <input type="text" name="keterangan" value="<?=$keterangan;?>" class="form-control" required>
+                                                                <br>
+                                                                <input type="number" name="quantity" value="<?=$quantity;?>" class="form-control" required>
+                                                                <br>
+                                                                <input type="hidden" name="idb" value="<?=$idb;?>">
+                                                                <input type="hidden" name="idm" value="<?=$idm;?>">
+                                                                <button type="submit" class="btn btn-primary" name="updatebarangmasuk">Submit</button>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="modal fade" id="delete<?=$idm;?>">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Hapus Barang?</h1>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+
+                                                        <form method="post">
+                                                            <div class="modal-body">
+                                                                Apakah Anda yakin ingin menghapus <?=$nama_kamera;?>?
+                                                                <input type="hidden" name="idb" value="<?=$idb;?>">
+                                                                <input type="hidden" name="quantity" value="<?=$quantity;?>">
+                                                                <input type="hidden" name="idm" value="<?=$idm;?>">
+                                                                <br>
+                                                                <br>
+                                                                <button type="submit" class="btn btn-danger" name="hapusbarangmasuk">Hapus</button>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        <?php
+                                        }
+                                        ?>
+
 
                                     </tbody>
                                 </table>
                             </div>
                         </div>
                     </div>
-                    </div>
-                </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2020</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
                         </div>
                     </div>
-                </footer>
-            </div>
+                </div>
+            </footer>
         </div>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/datatables-demo.js"></script>
-    </body>
+    </div>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/datatables-demo.js"></script>
+</body>
 
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -151,18 +208,18 @@ require 'tes-koneksi.php';
                 <div class="modal-body">
                     <select name="kameranya" class="form-control">
                         <?php
-                            $ambilsemuadatanya = mysqli_query($koneksi, "SELECT * FROM stock");
-                            while($fetcharray = mysqli_fetch_array($ambilsemuadatanya)){
-                                $kameranya = $fetcharray['nama_kamera'];
-                                $id_kamera = $fetcharray['id_kamera'];
-                            
-                            ?>
+                        $ambilsemuadatanya = mysqli_query($koneksi, "SELECT * FROM stock");
+                        while ($fetcharray = mysqli_fetch_array($ambilsemuadatanya)) {
+                            $kameranya = $fetcharray['nama_kamera'];
+                            $id_kamera = $fetcharray['id_kamera'];
 
-                            <option value="<?=$id_kamera;?>"><?=$kameranya;?></option>
+                        ?>
+
+                            <option value="<?= $id_kamera; ?>"><?= $kameranya; ?></option>
 
 
-                            <?php
-                            }
+                        <?php
+                        }
                         ?>
                     </select>
                     <br>
@@ -176,6 +233,6 @@ require 'tes-koneksi.php';
         </div>
     </div>
 </div>
-    
+
 
 </html>
